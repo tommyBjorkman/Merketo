0c624d72fdfbee21ae4e85f2b7f3de84c670b611
tommyBjorkman <tommy.bjorkman@utb.ecutbildning.se>
2023-05-22 20:04:21 +0200

    added and started editing controllers

---
+++ WebApp/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [Route("products/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("products/{id}")]
+        public IActionResult Detail(string id)
+        {
+            return View();
+        }
+    }
+}
