527b76b4ade7f88a78b5cc72a96037796d733ecf
tommyBjorkman <tommy.bjorkman@utb.ecutbildning.se>
2023-05-23 18:29:30 +0200

    edited accountcontroller, added regions

--- WebApp/Controllers/AccountController.cs
+++ WebApp/Controllers/AccountController.cs
@@ -4,9 +4,42 @@ namespace WebApp.Controllers
 {
     public class AccountController : Controller
     {
+        #region My Account (https://domain.com/account)
         public IActionResult Index()
         {
             return View();
         }
+        #endregion
+
+        #region Register (https://domain.com/account/register)
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        #endregion
+
+        #region Login (https://domain.com/account/login)
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        #endregion
+
+        #region Logout (https://domain.com/account/logout)
+        public IActionResult Logout()
+        {
+            return View();
+        }
+        #endregion
     }
 }
